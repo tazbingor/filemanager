c377a8afdfd145d21fa7ae0c0478ad13a231285a
tazbingo <tazdingo@qq.com>
2017-10-05 22:07:57 +0800

    func read file

--- dir.func.php
+++ dir.func.php
@@ -7,3 +7,26 @@
  *
  * 所有目录相关操作
  */
+
+//打开目录
+$handle = opendir($path);
+//读取目录
+while (($item = readdir($handle)) !== false) {
+
+    //除去.或者..文件名的目录
+    if ($item = '.' && $item != '..') {
+
+        //若是文件
+        if (is_file($path . "/" . $item)) {
+            $arr['file'][] = $item;
+        }
+
+        //若是目录
+        if (is_dir($path . "/" . $item)) {
+            $arr['dir'][] = $item;
+        }
+    }
+}
+
+//关闭
+closedir($handle);
