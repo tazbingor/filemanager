9625126862eeb4dc84d17d781682db2518932d5a
tazbingo <tazdingo@qq.com>
2017-10-07 18:23:52 +0800

    文件操作,判断文件单位func

---
+++ file.func.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tazbingo
+ * Date: 2017/10/7
+ * Time: 18:04
+ */
+
+/**
+ * @param $size
+ * @return string
+ * 判断文件单位
+ */
+function transByte($size)
+{
+    $i = 0;//默认Byte
+//    $size = 200e5;
+    $arr = array(
+        "Byte",
+        "KB",
+        "MB",
+        "G",
+        "T",
+        "E"
+    );
+
+
+    while ($size >= 1024) {
+        $size /= 1024;
+        $i++;
+    }
+    return round($size, 2) . $arr[$i];
+}
