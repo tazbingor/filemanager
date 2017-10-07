99d077d57afa693e0907e940fc3cc5c8c3b53d5c
tazbingo <tazdingo@qq.com>
2017-10-08 02:19:16 +0800

    新建文件夹页完成

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
 require_once 'dir.func.php';
 require_once 'file.func.php';
 $path = "file";
+$act = $_REQUEST['act'];
+$filename = $_REQUEST['filename'];
 $info = readDirectory($path);
 
 //    print_r($info);
@@ -24,51 +26,10 @@ $info = readDirectory($path);
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <title>文件管理器</title>
-    <link rel="stylesheet" href="cikonss.css"/>
+    <link rel="stylesheet" href="css/cikonss.css"/>
     <link rel="stylesheet" href="jquery-ui/css/ui-lightness/jquery-ui-1.10.4.custom.css" type="text/css"/>
-    <style type="text/css">
-        body, p, div, ul, ol, table, dl, dd, dt {
-            margin: 0;
-            padding: 0;
-        }
-
-        a {
-            text-decoration: none;
-        }
-
-        ul, li {
-            list-style: none;
-            float: left;
-        }
-
-        #top {
-            width: 100%;
-            height: 48px;
-            margin: 0 auto;
-            background: #E2E2E2;
-        }
-
-        #navi a {
-            display: block;
-            width: 48px;
-            height: 48px;
-        }
-
-        #main {
-            margin: 0 auto;
-            border: 2px solid #ABCDEF;
-        }
-
-        .small {
-            width: 25px;
-            height: 25px;
-            border: 0;
-        }
-    </style>
-    <script type="text/javascript">
-
-        //todo
-    </script>
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/main.js"></script>
 </head>
 <body>
 
@@ -97,104 +58,164 @@ $info = readDirectory($path);
     </ul>
 </div>
 
-<table width="100%" border="1" cellpadding="5" cellspacing="0" bgcolor="#ABCDEF" align="center">
-    <tr>
-        <td>编号</td>
-        <td>名称</td>
-        <td>类型</td>
-        <td>大小</td>
-        <td>可读</td>
-        <td>可写</td>
-        <td>可执行</td>
-        <td>创建时间</td>
-        <td>修改时间</td>
-        <td>访问时间</td>
-        <td>操作</td>
-    </tr>
-    <?php
-    if ($info['file']) {
-        $i = 1;
-        foreach ($info['file'] as $value) {
-            $p = $path . "/" . $value;
-            ?>
-            <tr>
-                <td><?php echo $i; ?></td>
-                <td><?php echo $value; ?></td>
-                <td><?php $src = filetype($p) == "file" ? "file_ico.png" : "folder_ico.png"; ?><img
-                            src="images/<?php echo $src; ?>" alt="" title="文件"/>
-                </td>
-                <td><?php echo transByte(filesize($p)); ?></td>
-                <td><?php $src = is_readable($p) ? "correct.png" : "error.png" ?><img class="small"
-                                                                                      src="images/<?php echo $src; ?>"
-                                                                                      alt=""></td>
-                <td><?php $src = is_writable($p) ? "correct.png" : "error.png" ?><img class="small"
-                                                                                      src="images/<?php echo $src; ?>"
-                                                                                      alt="">
-                </td>
-                <td><?php $src = is_executable($p) ? "correct.png" : "error.png" ?><img class="small"
-                                                                                        src="images/<?php echo $src; ?>"
-                                                                                        alt="">
-                </td>
-                <td><?php echo date("Y-m-d H:i:s", filectime($p)) ?></td>
-                <td><?php echo date("Y-m-d H:i:s", filemtime($p)) ?></td>
-                <td><?php echo date("Y-m-d H:i:s", fileatime($p)) ?></td>
+<form action="index.php" method="post" enctype="multipart/form-data">
+    <table width="100%" border="1" cellpadding="5" cellspacing="0" bgcolor="#ABCDEF" align="center">
 
-                <td>
-                    <?php
+        <tr id="createFolder" style="display:none;">
+            <td>请输入文件夹名称</td>
+            <td>
+                <input type="text" name="dirname"/>
+                <input type="hidden" name="path" value="<?php echo $path; ?>"/>
+                <input type="submit" name="act" value="创建文件夹"/>
+            </td>
+        </tr>
 
-                    //得到文件扩展名
-                    $s = explode(".", $value);
-                    $endS = end($s);
-                    $ext = strtolower($endS);
-                    $imageExt = array("gif", "jpg", "jpeg", "png");
-                    if (in_array($ext, $imageExt)) {
-                        ?>
-                        <a href="#" onclick="showDetail('<?php echo $value; ?>','<?php echo $p; ?>')"><img class="small"
-                                                                                                           src="images/show.png"
-                                                                                                           alt=""
-                                                                                                           title="查看"/></a>|
+        <tr>
+            <td>编号</td>
+            <td>名称</td>
+            <td>类型</td>
+            <td>大小</td>
+            <td>可读</td>
+            <td>可写</td>
+            <td>可执行</td>
+            <td>创建时间</td>
+            <td>修改时间</td>
+            <td>访问时间</td>
+            <td>操作</td>
+        </tr>
+        <?php
+        if ($info['file']) {
+            $i = 1;
+            foreach ($info['file'] as $value) {
+                $p = $path . "/" . $value;
+                ?>
+                <tr>
+                    <td><?php echo $i; ?></td>
+                    <td><?php echo $value; ?></td>
+                    <td><?php $src = filetype($p) == "file" ? "file_ico.png" : "folder_ico.png"; ?><img
+                                src="images/<?php echo $src; ?>" alt="" title="文件"/>
+                    </td>
+                    <td><?php echo transByte(filesize($p)); ?></td>
+                    <td><?php $src = is_readable($p) ? "correct.png" : "error.png" ?><img class="small"
+                                                                                          src="images/<?php echo $src; ?>"
+                                                                                          alt=""></td>
+                    <td><?php $src = is_writable($p) ? "correct.png" : "error.png" ?><img class="small"
+                                                                                          src="images/<?php echo $src; ?>"
+                                                                                          alt="">
+                    </td>
+                    <td><?php $src = is_executable($p) ? "correct.png" : "error.png" ?><img class="small"
+                                                                                            src="images/<?php echo $src; ?>"
+                                                                                            alt="">
+                    </td>
+                    <td><?php echo date("Y-m-d H:i:s", filectime($p)) ?></td>
+                    <td><?php echo date("Y-m-d H:i:s", filemtime($p)) ?></td>
+                    <td><?php echo date("Y-m-d H:i:s", fileatime($p)) ?></td>
+
+                    <td>
                         <?php
-                    } else {
-                        ?>
-                        <a href="index.php?act=showContent&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
-                                    class="small" src="images/show.png" alt="" title="查看"/></a>|
-                    <?php } ?>
-                    <a href="index.php?act=editContent&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
-                                class="small" src="images/edit.png" alt="" title="修改"/></a>|
-                    <a href="index.php?act=renameFile&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
-                                class="small" src="images/rename.png" alt="" title="重命名"/></a>|
-                    <a href="index.php?act=copyFile&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
-                                class="small"
-                                src="images/copy.png"
-                                alt=""
-                                title="复制"/></a>|
-                    <a href="index.php?act=cutFile&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
-                                class="small"
-                                src="images/cut.png"
-                                alt=""
-                                title="剪切"/></a>|
-                    <a href="#" onclick="delFile('<?php echo $p; ?>','<?php echo $path; ?>')"><img class="small"
-                                                                                                   src="images/delete.png"
-                                                                                                   alt=""
-                                                                                                   title="删除"/></a>|
-                    <a href="index.php?act=downFile&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
-                                class="small"
-                                src="images/download.png"
-                                alt=""
-                                title="下载"/></a>
-                </td>
+
+                        //得到文件扩展名
+                        $s = explode(".", $value);
+                        $endS = end($s);
+                        $ext = strtolower($endS);
+                        $imageExt = array("gif", "jpg", "jpeg", "png");
+                        if (in_array($ext, $imageExt)) {
+                            ?>
+                            <a href="#" onclick="showDetail('<?php echo $value; ?>','<?php echo $p; ?>')"><img
+                                        class="small"
+                                        src="images/show.png"
+                                        alt=""
+                                        title="查看"/></a>|
+                            <?php
+                        } else {
+                            ?>
+                            <a href="index.php?act=showContent&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
+                                        class="small" src="images/show.png" alt="" title="查看"/></a>|
+                        <?php } ?>
+                        <a href="index.php?act=editContent&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
+                                    class="small" src="images/edit.png" alt="" title="修改"/></a>|
+                        <a href="index.php?act=renameFile&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
+                                    class="small" src="images/rename.png" alt="" title="重命名"/></a>|
+                        <a href="index.php?act=copyFile&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
+                                    class="small"
+                                    src="images/copy.png"
+                                    alt=""
+                                    title="复制"/></a>|
+                        <a href="index.php?act=cutFile&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
+                                    class="small"
+                                    src="images/cut.png"
+                                    alt=""
+                                    title="剪切"/></a>|
+                        <a href="#" onclick="delFile('<?php echo $p; ?>','<?php echo $path; ?>')"><img class="small"
+                                                                                                       src="images/delete.png"
+                                                                                                       alt=""
+                                                                                                       title="删除"/></a>|
+                        <a href="index.php?act=downFile&path=<?php echo $path; ?>&filename=<?php echo $p; ?>"><img
+                                    class="small"
+                                    src="images/download.png"
+                                    alt=""
+                                    title="下载"/></a>
+                    </td>
 
 
-            </tr>
+                </tr>
 
 
-            <?php
-            $i++;
+                <?php
+                $i++;
+            }
         }
-    }
-    ?>
-</table>
+        ?>
 
+        <!-- 读取目录的操作-->
+        <?php
+        if ($info['dir']) {
+            $i = $i == null ? 1 : $i;
+            foreach ($info['dir'] as $val) {
+                $p = $path . "/" . $val;
+                ?>
+                <tr>
+                    <td><?php echo $i; ?></td>
+                    <td><?php echo $val; ?></td>
+                    <td><?php $src = filetype($p) == "file" ? "file_ico.png" : "folder_ico.png"; ?><img
+                                src="images/<?php echo $src; ?>" alt="" title="文件"/></td>
+                    <td><?php $sum = 0;
+                        echo transByte(dirSize($p)); ?></td>
+                    <td><?php $src = is_readable($p) ? "correct.png" : "error.png"; ?><img class="small"
+                                                                                           src="images/<?php echo $src; ?>"
+                                                                                           alt=""/></td>
+                    <td><?php $src = is_writable($p) ? "correct.png" : "error.png"; ?><img class="small"
+                                                                                           src="images/<?php echo $src; ?>"
+                                                                                           alt=""/></td>
+                    <td><?php $src = is_executable($p) ? "correct.png" : "error.png"; ?><img class="small"
+                                                                                             src="images/<?php echo $src; ?>"
+                                                                                             alt=""/></td>
+                    <td><?php echo date("Y-m-d H:i:s", filectime($p)); ?></td>
+                    <td><?php echo date("Y-m-d H:i:s", filemtime($p)); ?></td>
+                    <td><?php echo date("Y-m-d H:i:s", fileatime($p)); ?></td>
+                    <td>
+                        <a href="index.php?path=<?php echo $p; ?>"><img class="small" src="images/show.png" alt=""
+                                                                        title="查看"/></a>|
+                        <a href="index.php?act=renameFolder&path=<?php echo $path; ?>&dirname=<?php echo $p; ?>"><img
+                                    class="small" src="images/rename.png" alt="" title="重命名"/></a>|
+                        <a href="index.php?act=copyFolder&path=<?php echo $path; ?>&dirname=<?php echo $p; ?>"><img
+                                    class="small" src="images/copy.png" alt="" title="复制"/></a>|
+                        <a href="index.php?act=cutFolder&path=<?php echo $path; ?>&dirname=<?php echo $p; ?>"><img
+                                    class="small" src="images/cut.png" alt="" title="剪切"/></a>|
+                        <a href="#" onclick="delFolder('<?php echo $p; ?>','<?php echo $path; ?>')"><img class="small"
+                                                                                                         src="images/delete.png"
+                                                                                                         alt=""
+                                                                                                         title="删除"/></a>|
+                    </td>
+                </tr>
+                <?php
+                $i++;
+            }
+        }
+
+        ?>
+    </table>
+</form>
 
 <script src="jquery-ui/js/jquery-1.10.2.js"></script>
 <script src="jquery-ui/js/jquery-ui-1.10.4.custom.js"></script>
