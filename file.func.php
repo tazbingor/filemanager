a77532854041513872b243d5e92b51d437222815
xb <852353298@qq.com>
2017-10-18 11:46:27 +0800

    上传文件

--- file.func.php
+++ file.func.php
@@ -157,5 +157,58 @@ function cutFile($filename,$dstname){
     return $mes;
 }
 
+/**
+ * 上传文件
+ * @param array $fileInfo
+ * @param string $path
+ * @param array $allowExt
+ * @param int $maxSize
+ * @return string
+ */
+function uploadFile($fileInfo,$path,$allowExt=array("gif","jpeg","jpg","png","txt"),$maxSize=10485760){
+    //判断错误号
+    if($fileInfo['error']==UPLOAD_ERR_OK){
+        //文件是否是通过HTTP POST方式上传上来的
+        if(is_uploaded_file($fileInfo['tmp_name'])){
+            //上传文件的文件名，只允许上传jpeg|jpg、png、gif、txt的文件
+            //$allowExt=array("gif","jpeg","jpg","png","txt");
+            $ext=getExt($fileInfo['name']);
+            $uniqid=getUniqidName();
+            $destination=$path."/".pathinfo($fileInfo['name'],PATHINFO_FILENAME)."_".$uniqid.".".$ext;
+            if(in_array($ext,$allowExt)){
+                if($fileInfo['size']<=$maxSize){
+                    if(move_uploaded_file($fileInfo['tmp_name'], $destination)){
+                        $mes="文件上传成功";
+                    }else{
+                        $mes="文件移动失败";
+                    }
+                }else{
+                    $mes="文件过大";
+                }
+            }else{
+                $mes="非法文件类型";
+            }
+        }else{
+            $mes="文件不是通过HTTP POST方式上传上来的";
+        }
+    }else{
+        switch($fileInfo['error']){
+            case 1:
+                $mes="超过了配置文件的大小";
+                break;
+            case 2:
+                $mes="超过了表单允许接收数据的大小";
+                break;
+            case 3:
+                $mes="文件部分被上传";
+                break;
+            case 4:
+                $mes="没有文件被上传";
+                break;
+        }
+    }
 
+    return $mes;
+
+}
 
